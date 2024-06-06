ade8dd0496ef11135e42255236bf0c90d279295b
Afolabi <151972237+walethewave@users.noreply.github.com>
2024-06-06 02:09:46 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# PostgresSQL 16
+# Sample
+# user = "postgres"
+# password = "geography5"
+# host = "localhost"
+# port = "5432"
+
+user = "your username here" #default psql username is postgres
+password = "your password here"
+host = "localhost" #default psql port is localhost or 127.0.0.1
+port = "your port here" #default psql port is 5432
